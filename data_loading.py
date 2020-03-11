980e1d5ad77b80b8880c120fcf8098d0818862a5
guillaume-chevalier <guillaume.chevalier@neuraxio.com>
2020-03-11 02:34:00 -0400

    Coded Kata 1

---
+++ data_loading.py
@@ -0,0 +1,88 @@
+import numpy as np
+
+# Useful Constants
+DATA_PATH = "data/"
+DATASET_PATH = DATA_PATH + "UCI HAR Dataset/"
+
+# Those are separate normalised input features for the neural network
+INPUT_SIGNAL_TYPES = [
+    "body_acc_x_",
+    "body_acc_y_",
+    "body_acc_z_",
+    "body_gyro_x_",
+    "body_gyro_y_",
+    "body_gyro_z_",
+    "total_acc_x_",
+    "total_acc_y_",
+    "total_acc_z_"
+]
+
+# Output classes to learn how to classify
+LABELS = [
+    "WALKING",
+    "WALKING_UPSTAIRS",
+    "WALKING_DOWNSTAIRS",
+    "SITTING",
+    "STANDING",
+    "LAYING"
+]
+TRAIN = "train/"
+TEST = "test/"
+
+
+def load_all_data():
+    # Load "X" (the neural network's training and testing inputs)
+
+    def load_X(X_signals_paths):
+        X_signals = []
+
+        for signal_type_path in X_signals_paths:
+            file = open(signal_type_path, 'r')
+            # Read dataset from disk, dealing with text files' syntax
+            X_signals.append(
+                [np.array(serie, dtype=np.float32) for serie in [
+                    row.replace('  ', ' ').strip().split(' ') for row in file
+                ]]
+            )
+            file.close()
+
+        return np.transpose(np.array(X_signals), (1, 2, 0))
+
+    X_train_signals_paths = [
+        DATASET_PATH + TRAIN + "Inertial Signals/" + signal + "train.txt" for signal in INPUT_SIGNAL_TYPES
+    ]
+    X_test_signals_paths = [
+        DATASET_PATH + TEST + "Inertial Signals/" + signal + "test.txt" for signal in INPUT_SIGNAL_TYPES
+    ]
+
+    X_train = load_X(X_train_signals_paths)
+    X_test = load_X(X_test_signals_paths)
+
+    # Load "y" (the neural network's training and testing outputs)
+
+    def load_y(y_path):
+        file = open(y_path, 'r')
+        # Read dataset from disk, dealing with text file's syntax
+        y_ = np.array(
+            [elem for elem in [
+                row.replace('  ', ' ').strip().split(' ') for row in file
+            ]],
+            dtype=np.int32
+        )
+        file.close()
+
+        # Substract 1 to each output class for friendly 0-based indexing
+        return y_ - 1
+
+    y_train_path = DATASET_PATH + TRAIN + "y_train.txt"
+    y_test_path = DATASET_PATH + TEST + "y_test.txt"
+
+    y_train = load_y(y_train_path)
+    y_test = load_y(y_test_path)
+
+    print("Some useful info to get an insight on dataset's shape and normalisation:")
+    print("(X shape, y shape, every X's mean, every X's standard deviation)")
+    print(X_test.shape, y_test.shape, np.mean(X_test), np.std(X_test))
+
+    return X_train, y_train, X_test, y_test
+
